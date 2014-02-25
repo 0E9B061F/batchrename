a29d3a778f230d579320d6ffb4fbc25556194e54
Macquarie Sharpless <macquarie.sharpless@gmail.com>
2014-02-24 19:01:24 -0500

    Add graphics to data_files in setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,12 @@
 from distutils.core import setup
 import py2exe
 
-setup(windows=[{
-	'script': 'br.py',
-	"icon_resources": [(1, "icon.ico")]
-}])
+icons = [('images', ['./icon16.png', './icon32.png', './icon64.png', './logo.png', './xquit.png'])]
+
+setup(
+	windows=[{
+		'script': 'br.py',
+		"icon_resources": [(1, "icon.ico")]
+	}],
+	data_files = icons
+)
